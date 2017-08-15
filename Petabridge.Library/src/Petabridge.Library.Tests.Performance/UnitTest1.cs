7ccaa3fb54b80d3d2e43a4659206ce47474ee85c
Aaron Stannard <aaron@petabridge.com>
2017-08-14 19:28:24 -0500

    initial project setup

---
+++ Petabridge.Library/src/Petabridge.Library.Tests.Performance/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Petabridge.Library.Tests.Performance
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
