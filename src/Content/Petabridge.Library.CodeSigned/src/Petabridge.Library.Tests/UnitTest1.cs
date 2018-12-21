d90f01bb1ed77002514007422cb163c87fe918c5
Aaron Stannard <aaron@petabridge.com>
2018-12-21 16:11:22 -0600

    added code-signed template

---
+++ src/Content/Petabridge.Library.CodeSigned/src/Petabridge.Library.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Petabridge.Library.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
